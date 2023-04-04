3f14f3ecfc1b179c85653f37b8970281e97b25d1
Reece M Whitelaw <rebeliongaming@gmail.com>
2023-04-04 00:03:47 -0700

    Shoot'em Up Finished
    Shoot'em Up Finished

---
+++ WEEK 9/Tower Defense Game/Assets/Scripts/LivesUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class LivesUI : MonoBehaviour
+{
+    public TextMeshProUGUI livesText;
+    private void Update() {
+        livesText.text = PlayerStats.Lives + " LIVES";
+    }
+}
